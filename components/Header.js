c8f74184cc0de99f1ad0e4668fcb605d34c45a28
PedroEspinDev <pedroesping@gmail.com>
2024-02-13 20:23:31 +0100

    Adding profile picture to sidebar

--- components/Header.js
+++ components/Header.js
@@ -5,8 +5,10 @@ import { MdOutlineOndemandVideo, MdOutlineExpandMore } from "react-icons/md";
 import { AiOutlineShop, AiFillMessage, AiFillBell } from "react-icons/ai";
 import { IoGameControllerOutline } from "react-icons/io5";
 import { CgMenuGridO } from "react-icons/cg";
+import { signOut, useSession } from "next-auth/react";
 
 const Header = () => {
+  const { data: session } = useSession();
   return (
     <div className="bg-white flex items-center p-2 shadow-md top-0 sticky z-50 h16">
       {/* Left */}
@@ -66,13 +68,12 @@ const Header = () => {
       </div>
       {/* Right */}
       <div className="flex items-center justify-end min-w-fit space-x-2">
-        <img
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/2021_Facebook_icon.svg/800px-2021_Facebook_icon.svg.png"
-          height={40}
-          width={40}
-        />
+        <img 
+        onClick={signOut}
+        src={session?.user.image} height={40} width={40} 
+        className="rounded-full cursor-pointer"/>
         <p className="hidden xl:inline-flex font-semibold texr-sm whitespace-nowrap p-3 max-w-xs">
-          Pedro Espín
+          {session?.user.name.split(" ")[0]}
         </p>
         <CgMenuGridO
           size={20}
